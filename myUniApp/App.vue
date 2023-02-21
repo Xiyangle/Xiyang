0376ece9658ecef656197aa18cbdcb24b076241a
Xiyangle <xiyangle@vip.qq.com>
2023-02-21 15:38:57 +0800

    更新代码

--- myUniApp/App.vue
+++ myUniApp/App.vue
@@ -1,14 +1,43 @@
 <script>
 	export default {
 		onLaunch: function() {
-			console.warn('当前组件仅支持 uni_modules 目录结构 ，请升级 HBuilderX 到 3.1.0 版本以上！')
 			console.log('App Launch')
 		},
 		onShow: function() {
 			console.log('App Show')
+			this.updateApp()
 		},
 		onHide: function() {
 			console.log('App Hide')
+		},
+		methods: {
+			updateApp(){
+				const updateManager = uni.getUpdateManager()
+				updateManager.onCheckForUpdate(function (res) {
+				  // 请求完新版本信息的回调
+				  if (res.hasUpdate) {
+				  	updateManager.onUpdateReady(function (res) {
+				  	  uni.showModal({
+				  	    title: '更新提示',
+				  	    content: '新版本已经准备好，是否重启应用？',
+				  	    success(res) {
+				  	      if (res.confirm) {
+				  	        // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+				  	        updateManager.applyUpdate()
+				  	      }
+				  	    }
+				  	  })
+				  	})
+				  	updateManager.onUpdateFailed(function (res) {
+				  	  // 新的版本下载失败
+					  uni.showModal({
+					  	title: '提示',
+						content: '检查到有新版本，但是下载失败，请检查网络设置'
+					  })
+				  	})
+				  }
+				})
+			}
 		}
 	}
 </script>
---
+++ myUniApp/pages/tabBar/index/index.vue
@@ -0,0 +1,47 @@
+<template>
+	<view style="width: 750rpx;">
+		<movable-area style="width: 100%;height: 90vh;">
+			<movable-view 
+				class="movable-view-box"
+				direction="all"
+				:x="x" :y="y"
+				:inertia="true"
+				@change="movableChange">
+				mystic
+			</movable-view>
+		</movable-area>
+		<view style="color: #f40;">
+			<view>X：{{x}}</view>
+			<view>Y：{{y}}</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				x: 0,
+				y: 0,
+			}
+		},
+		methods: {
+			movableChange(e){
+				this.x = e.detail.x
+				this.y = e.detail.y
+			}
+		}
+	}
+</script>
+
+<style>
+	.movable-view-box{
+		height: 100rpx;
+		width: 120rpx;
+		background-color: #f40;
+		text-align: center;
+		line-height: 100rpx;
+		border-radius: 8rpx;
+		color: #fff;
+	}
+</style>
