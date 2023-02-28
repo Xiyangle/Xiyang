227f28ed2ed727c5f40e4124e94f106b95ad9bd0
Xiyangle <xiyangle@vip.qq.com>
2023-02-28 13:26:24 +0800

    更新

--- myUniApp/pages/tabBar/list/list.vue
+++ myUniApp/pages/tabBar/list/list.vue
@@ -1,16 +1,28 @@
 <template>
 	<view class="">
-		
+		<uni-indexed-list :options="list" :showSelect="false" @click="bindClick"></uni-indexed-list>
 	</view>
 </template>
 
 <script>
+	import airport from '@/utils/apiData.js'
 	export default {
 		data() {
 			return {
-				
+				list: airport.list
 			}
 		},
+		onPullDownRefresh() {
+			setTimeout(() =>{
+				console.log(airport.list)
+				uni.stopPullDownRefresh()
+			},1000)
+		},
+		methods: {
+			bindClick(e){
+				console.log(e)
+			}
+		}
 	}
 </script>
 
